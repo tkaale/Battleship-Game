69d8c8c77c102ef957d938c2b9a260ca32188567
Aleksandra <tkaczyk.aleksandra@outlook.com>
2022-03-04 10:30:13 +0100

    ask user for coordinates and convert it into index numbers

--- game.py
+++ game.py
@@ -1,26 +1,67 @@
-import os
-os.system("")
 import data_menager
-
-def print_red(skk): print("\033[91m{}\033[00m" .format(skk))
+import ui
 
 BOARD_PLR_ONE = data_menager.get_table_from_file('Battleship-Game/board_one.cvs') 
-BOARD_PLR_TWO = data_menager.get_table_from_file('Battleship-Game/board_two.cvs')\
+BOARD_PLR_TWO = data_menager.get_table_from_file('Battleship-Game/board_two.cvs')
+
+
+COL = 1
+ROW = 0
 
 def print_board(board, player):
-    print_red('\n ', player, '\n')
+    ui.print_red('\n ', player, '\n')
     for sublist in board:
         print(' '.join(sublist))
 
 def print_two_boards(board_one, board_two):
-    print_red('\n PLAYER ONE', end = '\t\t')
-    print_red(' PLAYER TWO\n')
+    ui.print_red('\n PLAYER ONE', end = '\t\t')
+    ui.print_red(' PLAYER TWO\n')
     for i in range(0,6):
         print(' '.join(board_one[i]), end = "\t\t")
         print(' '.join(board_two[i]))
 
-def main():
+def col_title(board):
+    print(board)
+    col_title = board[0]
+    return col_title   #[' ', '1', '2', '3', '4', '5']
+
+def row_title(board):
+    row_title = []
+    for sublist in board:
+        row_title.append(sublist[0])
+    return row_title  #[' ', 'A', 'B', 'C', 'D', 'E']
+
+def user_coordinates(board):   
+    while True:
+        user_input = input('Please provide a coordinates: ').upper()
+        if len(user_input) < 2 or not user_input[ROW].isalpha() or not user_input[COL].isnumeric():
+            ui.print_red('\nWrong! You should write B4, A1, G7 etc etc.')
+            continue
+        else:
+            if user_input[ROW] not in row_title(board) or user_input[COL] not in col_title(board):
+                ui.print_red('Wrong! Try again')
+                continue
+            break
+    return user_input
+
+def user_coordinates_convert(board, user_input):
+    user_input_convert = []
+    for element in row_title(board):
+        if element == user_input[ROW]:
+            row_index = row_title(board).index(element)
+            user_input_convert.append(row_index)
+    for element in col_title(board):
+        if element == user_input[COL]:
+            col_index = col_title(board).index(element)
+            user_input_convert.append(col_index)
+    return user_input_convert
+
+
+def set_ships():
     pass
+
+def main():
+    ui.start_title()
 
 if __name__ == '__main__':
     main()
