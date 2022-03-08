cd7a5472a6a38d26f2e26145a61b236c1d94ead6
Aleksandra <tkaczyk.aleksandra@outlook.com>
2022-03-08 14:16:31 +0100

    section to set ships complete

--- game.py
+++ game.py
@@ -87,15 +87,13 @@ def available_ships(board_size, player):
     if board_size == 'small':
         ui.small_board_info(player)
 
-def ask_for_orientation(blocks, user_coordinate):
+def ask_for_orientation():
     while True:
-        user_input = input("Please enter H for HORIZONTAL (left to right) set or V for VERTICAL (up to bottom) set: ").upper()
+        user_input = input("\nPlease enter H for HORIZONTAL (left to right) or V for VERTICAL (up to bottom): ").upper()
         if user_input == 'H':
-            ship_cells = set_horizontal(blocks, user_coordinate)
-            return ship_cells
+            return user_input
         if user_input == 'V':
-            ship_cells = set_vertical(blocks, user_coordinate)
-            return ship_cells
+            return user_input
         else:
             ui.print_red("Wrong! You should write H or V!")
             continue
@@ -114,106 +112,125 @@ def set_vertical(blocks, user_coordinate):
         user_coordinate = [user_coordinate[ROW] + 1, user_coordinate[COL]]
     return ship_cells
 
-
-def set_ship_on_board(board, ship_cells):
+def set_ship_on_board(board, ship_cells, file_name):
     for element in ship_cells:
         coordinates_row = element[ROW]
         coordinates_col = element[COL]
         board[coordinates_row][coordinates_col] = 'X'
+    data_menager.overwrite_table_to_file(board, file_name)
     return board
 
 
-def setting_ship_big(board, player):
-    ships_list_big = {'Carrier (5 blocks)': 5, 'Battleship (4 blocks)': 4, 'Cruiser (3 blocks)': 3, 'Submarine (3 blocks)': 3, 'Submarine (3 blocks)': 3, 'Destroyer (2 blocks)': 2, 'Destroyer (2 blocks)': 2, 'Destroyer (2 blocks)': 2}
-    for boat in ships_list_big:
-        print_board(board, player)
+def blocking_places_near_ship_horizontal(ship_cells, board_size):
+    blocked_cells = []
+    for element in ship_cells:
+        blocked_cells.append([element[ROW] - 1, element[COL]])
+        blocked_cells.append([element[ROW] + 1, element[COL]])
+    first = [ship_cells[0]]
+    last = [ship_cells[-1]]
+    for element in first:
+        blocked_cells.append([element[ROW], element[COL] - 1])
+    for element in last:
+        blocked_cells.append([element[ROW], element[COL] + 1])
+    for element in blocked_cells:
+        for i in element:
+            if board_size == 'big':
+                if i > 9:
+                    blocked_cells.remove(element)
+            if board_size == 'small':
+                if i > 5:
+                    blocked_cells.remove(element)
+    return blocked_cells
+
+def blocking_places_near_ship_vertical(ship_cells, board_size):
+    blocked_cells = []
+    for element in ship_cells:
+        blocked_cells.append([element[ROW], element[COL] + 1])
+        blocked_cells.append([element[ROW], element[COL] - 1])
+    first = [ship_cells[0]]
+    last = [ship_cells[-1]]
+    for element in first:
+        blocked_cells.append([element[ROW] - 1, element[COL]])
+    for element in last:
+        blocked_cells.append([element[ROW] + 1, element[COL]])
+    for element in blocked_cells:
+        for i in element:
+            if board_size == 'big':
+                if i > 9:
+                    blocked_cells.remove(element)
+            if board_size == 'small':
+                if i > 5:
+                    blocked_cells.remove(element)
+    return blocked_cells
+
+def set_blocked_place(board, blocked_cells, file_name):
+    for element in blocked_cells:
+        coordinates_row = element[ROW]
+        coordinates_col = element[COL]
+        if board[coordinates_row][coordinates_col] in col_title(board) or board[coordinates_row][coordinates_col] in row_title(board) or board[coordinates_row][coordinates_col] in row_title(board) == 'X':
+            blocked_cells.remove(element)
+    for element in blocked_cells:
+        coordinates_row = element[ROW]
+        coordinates_col = element[COL]
+        board[coordinates_row][coordinates_col] = '-'
+    data_menager.overwrite_table_to_file(board, file_name)
+    return board
+
+def checking_coordinates(ship_cells, board_size):
+    for element in ship_cells:
+        for i in element:
+            if board_size == 'big':
+                if i > 9:
+                    return False
+            if board_size == 'small':
+                if i > 5:
+                    return False
+    else:
+        return True
+
+def check_available_place(board, coordinates):
+    coordinates_row = coordinates[ROW]
+    coordinates_col = coordinates[COL]
+    if board[coordinates_row][coordinates_col] == 'X' or board[coordinates_row][coordinates_col] == '-':
+        return False
+    else:
+        return True
+
+def setting_ship(board, player, board_size, ships_list, file_name):
+    print_board(board, player)
+    for boat in ships_list:
+        print(boat)
         title = (f"Enter starting coordinate (e.g. B4) for the {boat}: ")
-        blocks = (ships_list_big[boat])
-        user_coordinate = user_coordinates_convert(board, user_coordinates(board, title))
-        ship_cells = ask_for_orientation(blocks, user_coordinate)
-        board = set_ship_on_board(board, ship_cells)
-        print_board(board, player)
-
-setting_ship_big(board_ii, "player one") 
-
-
-def setting_ship_small(board, player):
-    ship_list_small = {'Submarine (3 blocks)': 3, 'Destroyer (2 blocks)': 2, 'Destroyer (2 blocks)': 2}
-
-
-
-
-# def setting_ships_info(board, player, blocks):
-#     print(f"\nIt's time for {player} to set ships.\nShips can be only 1-block long and 2-blocks long. You have {blocks} blocks to use.\nPress ENTER to continue")
-#     input()
-#     print_board(board, player)
-
-# def set_ships(player, blocks, board):
-#     setting_ships_info(board, player,blocks)
-#     ships = []
-#     while blocks > 0:
-#         ui.print_green(f"\nAmount of blocks to use: {blocks}")
-#         user_input = ui.choose_ship_option()
-#         while True:
-#             if user_input == '1':
-#                 coordinates = user_coordinates_convert(board, user_coordinates(board, "Please provide coordinates: ")) 
-#                 check_coordinates = check_if_place_available(board, coordinates)
-#                 if check_coordinates == True:
-#                     board = set_ship_on_board(board,coordinates)
-#                     print_board(board, player)
-#                     blocks -= 1
-#                     ships.append([coordinates])
-#                     break
-#                 break
-#             if user_input == '2':
-#                 if blocks == 1:
-#                     ui.print_red('\nYou can only set 1-block-long ship!')
-#                     break
-#                 else:
-#                     coordinates_one = user_coordinates_convert(board, user_coordinates(board, "Please provide start coordinates: "))
-#                     check_coordinates = check_if_place_available(board, coordinates_one)
-#                     if check_coordinates == True:
-#                         coordinates_two = user_coordinates_convert(board, user_coordinates  (board, "Please provide end coordinates: "))
-#                         check_coordinates = check_if_place_available(board, coordinates_two)
-#                         if check_coordinates == True:
-#                             check_coordinates_places = ui.blocking_more_than_2_blocks   (coordinates_one)
-#                             if coordinates_two in check_coordinates_places:
-#                                 board = set_ship_on_board(board, coordinates_one)
-#                                 board = set_ship_on_board(board, coordinates_two)
-#                                 print_board(board, player)
-#                                 blocks -= 2
-#                                 ships.append([coordinates_one, coordinates_two])
-#                                 break
-#                             else:
-#                                 ui.print_red("\nYou choose 2-block-long ship. \nYou have to set second block next to first block. Try again")
-#                         break
-#                     break
-#             else:
-#                 ui.print_red('\nThere is no such option.')
-#                 break
-#     return ships
-
-
-# def set_ship_on_board(board,coordinates):
-#     coordinates_row = coordinates[ROW]
-#     coordinates_col = coordinates[COL]
-#     board[coordinates_row][coordinates_col] = 'X'
-#     return board
-        
-# def check_if_place_available(board, coordinates):
-#     coordinates_row = coordinates[ROW]
-#     coordinates_col = coordinates[COL]
-#     if board[coordinates_row][coordinates_col] == 'X':
-#         ui.print_red('\nYou already set here a ship. Try again')
-#         return False
-#     else:
-#         return True
-
-
-
-
-
-
+        blocks = (ships_list[boat])
+        while True:
+            user_coordinate = user_coordinates_convert(board, user_coordinates(board, title))
+            if check_available_place(board, user_coordinate) == False:
+                ui.print_red("\nYou can't put here your ship. Try again")
+                continue
+            else:
+                orientation = ask_for_orientation()
+                if orientation == 'H':
+                    ship_cells = set_horizontal(blocks, user_coordinate)
+                    if checking_coordinates(ship_cells, board_size) == False:
+                        ui.print_red('\nThere is no space for this ship. Try again!')
+                        continue
+                    else:
+                        board = set_ship_on_board(board, ship_cells, file_name)
+                        blocked_cells = blocking_places_near_ship_horizontal(ship_cells, board_size)
+                        board = set_blocked_place(board, blocked_cells, file_name)
+                        print_board(board, player)
+                        break
+                if orientation == 'V':
+                    ship_cells = set_vertical(blocks, user_coordinate)
+                    if checking_coordinates(ship_cells, board_size) == False:
+                        ui.print_red('\nThere is no space for this ship. Try again!')
+                        continue
+                    else:
+                        board = set_ship_on_board(board, ship_cells, file_name)
+                        blocked_cells = blocking_places_near_ship_vertical(ship_cells, board_size)
+                        board = set_blocked_place(board, blocked_cells, file_name)
+                        print_board(board, player)
+                        break
 
 def confirm_ships():
     ui.print_green('\nPress ENTER to hide your ships')
@@ -222,18 +239,28 @@ def confirm_ships():
     for i in range(0,50):
         print('')
 
-# def main():
-#     ui.start_title()
-#     blocks = choose_board()
-#     board_plr_one = data_menager.get_table_from_file('Battleship-Game/board_one.cvs')
-#     board_plr_two = data_menager.get_table_from_file('Battleship-Game/board_two.cvs')
-#     ships_player_one = set_ships(PLAYER_ONE, blocks, board_plr_one)
-#     confirm_ships()
-    
-#     ships_player_two = set_ships(PLAYER_TWO, blocks, board_plr_two)
-#     confirm_ships()
-#     pass
+def main():
+    ui.start_title()
+    board_size = choose_board()
+    board_plr_one = data_menager.get_table_from_file('Battleship-Game/board_one.cvs')
+    board_plr_two = data_menager.get_table_from_file('Battleship-Game/board_two.cvs')
+    ships_list_big = {'Carrier (5 blocks)': 5, 'Battleship (4 blocks)': 4, 'Cruiser (3 blocks)': 3, 'Submarine (3 blocks)': 3, 'Submarine 2 (3 blocks)': 3, 'Destroyer (2 blocks)': 2, 'Destroyer 2 (2 blocks)': 2, 'Destroyer 3 (2 blocks)': 2}
+    ships_list_small = {'Submarine (3 blocks)': 3, 'Destroyer (2 blocks)': 2, 'Destroyer (2 blocks)': 2, 'Destroyer 2 (2 blocks)': 2}
+    if board_size == 'big':
+        available_ships(board_size, PLAYER_ONE)
+        setting_ship(board_plr_one, PLAYER_ONE, board_size, ships_list_big, FILE_BOARD_ONE)
+        confirm_ships()
+        available_ships(board_size, PLAYER_TWO)
+        setting_ship(board_plr_two, PLAYER_TWO, board_size, ships_list_big, FILE_BOARD_TWO)
+        confirm_ships()
+    if board_size == 'small':
+        available_ships(board_size, PLAYER_ONE)
+        setting_ship(board_plr_one, PLAYER_ONE, board_size, ships_list_small, FILE_BOARD_ONE)
+        confirm_ships()
+        available_ships(board_size, PLAYER_TWO)
+        setting_ship(board_plr_two, PLAYER_TWO, board_size, ships_list_small, FILE_BOARD_TWO)
+        confirm_ships()
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
 
